45706c0af3b85089520375b8a277cba085e19ea2
alberick45 <papakofi845@gmail.com>
2025-09-29 21:43:30 -1000

    added worldfile and controller

---
+++ controllers/desk_buddy_controller/desk_buddy_controller.py
@@ -0,0 +1,15 @@
+from controller import Robot
+
+# Time step (same as WorldInfo.basicTimeStep)
+TIME_STEP = 32
+
+# Initialize the robot
+robot = Robot()
+
+print("🤖 Robo Desk Buddy is alive!")
+
+
+def wave():
+    print ("waving")
+# Main loop
+wave()
